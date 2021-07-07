d2052cbc5c97e6b95731e7de26d0c3456ae0189b
Saif Khan <saifkhichi96@gmail.com>
2021-07-07 22:12:17 +0200

    Imported database module from https://github.com/saifkhichi96/android-database

--- app/src/main/java/com/saifkhichi/app/db/remote/ServiceModule.kt
+++ app/src/main/java/com/saifkhichi/app/db/remote/ServiceModule.kt
@@ -1,9 +1,13 @@
 package com.saifkhichi.app.db.remote
 
-import dagger.Binds
+import android.content.Context
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import io.github.saifkhichi96.android.db.FirebaseDatabase
+import io.github.saifkhichi96.android.db.RemoteDatabase
 
 /**
  * A module which informs the Hilt which [WebService] to use when
@@ -14,9 +18,16 @@ import dagger.hilt.android.components.ViewModelComponent
  */
 @Module
 @InstallIn(ViewModelComponent::class)
-abstract class ServiceModule {
-    @Binds
-    abstract fun bindWebService(
-        service: FirebaseDatabaseService, // use Firebase Realtime Database
-    ): WebService
+object ServiceModule {
+
+    @Provides
+    fun provideWebService(@ApplicationContext context: Context): WebService {
+        return FirebaseDatabaseService() // use Firebase Realtime Database
+    }
+
+    @Provides
+    fun provideRemoteDatabase(@ApplicationContext context: Context): RemoteDatabase {
+        return FirebaseDatabase(context, persistent = true)
+    }
+
 }
